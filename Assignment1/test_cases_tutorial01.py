388861ea2a29f922f8d6d94fdff47d146badf1a7
RaghavBhardwaj24 <raghhavbharadwaj@gmail.com>
2020-09-17 14:49:09 +0530

    added power function to the code

--- Assignment1/test_cases_tutorial01.py
+++ Assignment1/test_cases_tutorial01.py
@@ -1,6 +1,6 @@
 import tutorial01 as A1
 
-actual_answers = [9, 12, 28, 5]
+actual_answers = [9, 12, 80, 5, [2, 6, 18, 54, 162],[1/5,1/4,1/3,1/2,1,0,-1,-1/2,-1/3,-1/4],0]
 student_answers = []
 
 test_case_1 = A1.add(4, 5)
@@ -9,14 +9,42 @@ student_answers.append(test_case_1)
 test_case_2 = A1.subtract(14, 2)
 student_answers.append(test_case_2)
 
-test_case_3 = A1.multiply(7, 4)
+
+test_case_3 = A1.multiply(10, 8)
 student_answers.append(test_case_3)
 
-test_case_4 = A1.divide(25,5)
+test_case_4 = A1.divide(10, 2)
 student_answers.append(test_case_4)
 
+# Driver code 
+
+a = 2 # starting number 
+r = 3 # Common ratio 
+n = 5 # N th term to be find 
+
+gp = A1.printGP(a, r, n) 
+gp = list(gp) 
+student_answers.append(gp)
+
+print(gp)
 print(actual_answers)
 print(student_answers)
+
+a=5
+d=-1
+n=10
+hp= A1.printHP(a,d,n)
+hp=list(hp)
+student_answers.append(hp)
+
+print(hp)
+print(actual_answers)
+print(student_answers)
+
+num1='a'
+num2=0
+power=A1.power(num1,num2)
+student_answers.append(power)
 
 total_test_cases = len(actual_answers)
 count_of_correct_test_cases = 0
@@ -26,3 +54,4 @@ for x, y in zip(actual_answers, student_answers):
         count_of_correct_test_cases += 1
 
 print(f"Test Cases Passed = '{count_of_correct_test_cases}'  / '{total_test_cases}'")
+
--- Assignment1/tutorial01.py
+++ Assignment1/tutorial01.py
@@ -20,4 +20,37 @@ def divide(num1, num2):
 	else:
 		division=num1/num2
 		return division
+
+
+# Function to add power function
+# You cant use the inbuilt python function x ** y . Write your own function
+def power(num1, num2):# num1 ^ num2
+	x=isinstance(num1,float) or isinstance(num1,int)
+	y=isinstance(num2,float) or isinstance(num2,int)
+	if x and y:
+		power=1
+		if num1==0 and num2==0:
+			power=0
+		else:
+			for i in range(1,int(num2)+1):
+				power=num1*power
+		
+		return power
+	else:
+		power=0
+		return power
+
+# Python 3 program to print GP.  geometric Progression
+# You cant use the inbuilt python function. Write your own function
+def printGP(a, r, n):
+	
+
+# Python 3 program to print AP.  arithmetic Progression
+# You cant use the inbuilt python function. Write your own function
+def printAP(a, d, n):
+	
+
+# Python 3 program to print HP.   Harmonic Progression
+# You cant use the inbuilt python function. Write your own function
+def printHP(a, d, n):
 	
