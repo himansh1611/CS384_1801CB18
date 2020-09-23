5ec35acf879dfd6bd42449dc682c2b9ebd51047f
RaghavBhardwaj24 <raghhavbharadwaj@gmail.com>
2020-09-23 23:14:11 +0530

    added mse function to my code!

--- Assignment2/tutorial02.py
+++ Assignment2/tutorial02.py
@@ -1,5 +1,23 @@
 # All decimal 3 places
 
+# Function to compute mse. You cant use Python functions
+def mse(first_list, second_list):
+    # mse Logic
+    for i in range(len(first_list)):
+        if isinstance(first_list[i], str):
+            return 0
+    for i in range(len(second_list)):
+        if isinstance(second_list[i], str):
+            return 0
+    if len(first_list) != len(second_list):
+        return 0
+    else:
+        x = []
+        for i in range(len(first_list)):
+            x.append((first_list[i] - second_list[i]) ** 2)
+        mse_value = (summation(x) / len(first_list))
+    return mse_value
+
 # Function to compute mean
 def mean(first_list):
     # mean Logic 
