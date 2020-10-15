f449b25a002af8a7924a2bc478b266eaa6a4d081
RaghavBhardwaj24 <raghhavbharadwaj@gmail.com>
2020-10-15 20:49:45 +0530

    added course function to the code!

---
+++ Assignment3/tutorial03.py
@@ -0,0 +1,160 @@
+import csv
+import os
+import re
+
+def course():
+    # Read csv and process
+    if (os.path.exists(r"C:\Users\admin\CS384_1801CB18\Assignment3\analytics")):
+        shutil.rmtree(r"C:\Users\admin\CS384_1801CB18\Assignment3\analytics")
+    else:
+        pass
+    with open('studentinfo_cs384.csv') as file:
+        reader = csv.DictReader(file)
+        lines = [dict(row) for row in reader]
+
+    if (os.path.exists(r"C:\Users\admin\CS384_1801CB18\Assignment3\analytics/course")):
+        pass
+    else:
+        os.makedirs(r"C:\Users\admin\CS384_1801CB18\Assignment3\analytics/course")
+
+    base_path = r"C:\Users\admin\CS384_1801CB18\Assignment3\analytics/course"
+    for row in lines:
+        if (bool(re.match('^[A-Z]+$', row["id"][4:6])) and len(row["id"]) == 8 and bool(
+                re.match('^[0-9]+$', row["id"][0:2])) and bool(re.match('^[0-9]+$', row["id"][6:8]))):
+            branch_code = row["id"][4:6]
+            if (os.path.exists(os.path.join(base_path, branch_code.lower()))):
+                pass
+            else:
+                os.makedirs(os.path.join(base_path, branch_code.lower()))
+            if (row["id"][2:4] == "01"):
+                if (os.path.exists(os.path.join(base_path, branch_code.lower(), "btech"))):
+                    pass
+                else:
+                    os.makedirs(os.path.join(base_path, branch_code.lower(), "btech"))
+                filename = row["id"][0:2] + "_" + branch_code.lower() + "_btech.csv"
+                if (os.path.exists(os.path.join(base_path, branch_code.lower(), "btech", filename))):
+                    with open(os.path.join(base_path, branch_code.lower(), "btech", filename), "a+") as file:
+                        header = ["id", "full_name", "country", "email", "gender", "dob", "blood_group", "state"]
+                        writer = csv.DictWriter(file, fieldnames=header)
+                        writer.writerow(row)
+                else:
+                    with open(os.path.join(base_path, branch_code.lower(), "btech", filename), "a+") as file:
+                        header = ["id", "full_name", "country", "email", "gender", "dob", "blood_group", "state"]
+                        writer = csv.DictWriter(file, fieldnames=header)
+                        writer.writeheader()
+                        writer.writerow(row)
+            elif (row["id"][2:4] == "11"):
+                if (os.path.exists(os.path.join(base_path, branch_code.lower(), "mtech"))):
+                    pass
+                else:
+                    os.makedirs(os.path.join(base_path, branch_code.lower(), "mtech"))
+                filename = row["id"][0:2] + "_" + branch_code.lower() + "_mtech.csv"
+                if (os.path.exists(os.path.join(base_path, branch_code.lower(), "mtech", filename))):
+                    with open(os.path.join(base_path, branch_code.lower(), "mtech", filename), "a+") as file:
+                        header = ["id", "full_name", "country", "email", "gender", "dob", "blood_group", "state"]
+                        writer = csv.DictWriter(file, fieldnames=header)
+                        writer.writerow(row)
+                else:
+                    with open(os.path.join(base_path, branch_code.lower(), "mtech", filename), "a+") as file:
+                        header = ["id", "full_name", "country", "email", "gender", "dob", "blood_group", "state"]
+                        writer = csv.DictWriter(file, fieldnames=header)
+                        writer.writeheader()
+                        writer.writerow(row)
+            elif (row["id"][2:4] == "12"):
+                if (os.path.exists(os.path.join(base_path, branch_code.lower(), "msc"))):
+                    pass
+                else:
+                    os.makedirs(os.path.join(base_path, branch_code.lower(), "msc"))
+                filename = row["id"][0:2] + "_" + branch_code.lower() + "_msc.csv"
+                if (os.path.exists(os.path.join(base_path, branch_code.lower(), "msc", filename))):
+                    with open(os.path.join(base_path, branch_code.lower(), "msc", filename), "a+") as file:
+                        header = ["id", "full_name", "country", "email", "gender", "dob", "blood_group", "state"]
+                        writer = csv.DictWriter(file, fieldnames=header)
+                        writer.writerow(row)
+                else:
+                    with open(os.path.join(base_path, branch_code.lower(), "msc", filename), "a+") as file:
+                        header = ["id", "full_name", "country", "email", "gender", "dob", "blood_group", "state"]
+                        writer = csv.DictWriter(file, fieldnames=header)
+                        writer.writeheader()
+                        writer.writerow(row)
+            elif (row["id"][2:4] == "21"):
+                if (os.path.exists(os.path.join(base_path, branch_code.lower(), "phd"))):
+                    pass
+                else:
+                    os.makedirs(os.path.join(base_path, branch_code.lower(), "phd"))
+                filename = row["id"][0:2] + "_" + branch_code.lower() + "_phd.csv"
+                if (os.path.exists(os.path.join(base_path, branch_code.lower(), "phd", filename))):
+                    with open(os.path.join(base_path, branch_code.lower(), "phd", filename), "a+") as file:
+                        header = ["id", "full_name", "country", "email", "gender", "dob", "blood_group", "state"]
+                        writer = csv.DictWriter(file, fieldnames=header)
+                        writer.writerow(row)
+                else:
+                    with open(os.path.join(base_path, branch_code.lower(), "phd", filename), "a+") as file:
+                        header = ["id", "full_name", "country", "email", "gender", "dob", "blood_group", "state"]
+                        writer = csv.DictWriter(file, fieldnames=header)
+                        writer.writeheader()
+                        writer.writerow(row)
+            else:
+                filename = "misc.csv"
+                if (os.path.exists(os.path.join(base_path, filename))):
+                    with open(os.path.join(base_path, filename), "a+") as file:
+                        header = ["id", "full_name", "country", "email", "gender", "dob", "blood_group", "state"]
+                        writer = csv.DictWriter(file, fieldnames=header)
+                        writer.writerow(row)
+                else:
+                    with open(os.path.join(base_path, filename), "a+") as file:
+                        header = ["id", "full_name", "country", "email", "gender", "dob", "blood_group", "state"]
+                        writer = csv.DictWriter(file, fieldnames=header)
+                        writer.writeheader()
+                        writer.writerow(row)
+        else:
+            filename = "misc.csv"
+            if (os.path.exists(os.path.join(base_path, filename))):
+                with open(os.path.join(base_path, filename), "a+") as file:
+                    header = ["id", "full_name", "country", "email", "gender", "dob", "blood_group", "state"]
+                    writer = csv.DictWriter(file, fieldnames=header)
+                    writer.writerow(row)
+            else:
+                with open(os.path.join(base_path, filename), "a+") as file:
+                    header = ["id", "full_name", "country", "email", "gender", "dob", "blood_group", "state"]
+                    writer = csv.DictWriter(file, fieldnames=header)
+                    writer.writeheader()
+                    writer.writerow(row)
+
+    pass
+
+
+def country():
+    # Read csv and process
+    pass
+
+
+def email_domain_extract():
+    # Read csv and process
+    pass
+
+
+def gender():
+    # Read csv and process
+    pass
+
+
+def dob():
+    # Read csv and process
+    pass
+
+
+def state():
+    # Read csv and process
+    pass
+
+
+def blood_group():
+    # Read csv and process
+    pass
+
+
+# Create the new file here and also sort it in this function only.
+def new_file_sort():
+    # Read csv and process
+    pass
