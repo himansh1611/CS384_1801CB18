dbcafde52e2c138262e231970fea6f3872b3e777
himansh1611 <himansh.g11@gmail.com>
2020-12-06 01:25:42 +0530

    Added append , open_reqcsv functions

--- Projects/P1 Quiz_via_CSV/p1_main.py
+++ Projects/P1 Quiz_via_CSV/p1_main.py
@@ -24,3 +24,51 @@ def cont_down(t):
     for x in range(total_time_in_sec):
         quiz_usertimer = quiz_usertimer - 1
         time.sleep(1)
+
+
+def create_hash_pass(password):
+    q = uuid.uuid4().hex
+    hashed_pass = hashlib.sha256(q.encode() + password.encode()).hexdigest()
+    return hashed_pass + ":" + q
+
+
+def check_password(hashed_password, user_password):
+    hashpassword, q = hashed_password.split(":")
+    hashed_pass = hashlib.sha256(q.encode() + user_password.encode()).hexdigest()
+    if hashpassword == hashed_pass:
+        return True
+    else:
+        return False
+
+
+def open_reqdirectory(directory=".", pwd=pwd()):
+    if directory == ".":
+        return
+    try:
+        os.chdir(directory)
+    except:
+        os.mkdir(os.path.join(pwd, str(directory)))
+        os.chdir(directory)
+    return None
+
+
+def append(file_name, headers, elements):
+    if os.path.exists(file_name):
+        with open(file_name, "a", newline="") as output_file:
+            writer = csv.writer(output_file, delimiter=",")
+            writer.writerow(elements)
+    else:
+        with open(file_name, "w", newline="") as output_file:
+            writer = csv.writer(output_file, delimiter=",")
+            writer.writerow(headers)
+            writer.writerow(elements)
+
+
+def open_reqcsv(input_file_path):
+    data_list = []
+    with open(input_file_path, "r") as input_file:
+        reader = csv.DictReader(input_file)
+        headers = reader.fieldnames
+        for row in reader:
+            data_list.append(row)
+    return data_list
