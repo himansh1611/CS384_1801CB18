1797edd0494a08b65bb94fffa00f6e3ebe555c1c
RaghavBhardwaj24 <raghhavbharadwaj@gmail.com>
2020-12-06 02:56:58 +0530

    added exit_fun function and find_fun function

--- Projects/P2 Notepad_GUI/p2_main.py
+++ Projects/P2 Notepad_GUI/p2_main.py
@@ -86,3 +86,82 @@ def Save_as_file(event=None):
 
 file.add_command(label="Save as", compound=tk.LEFT,
                  accelerator="Ctrl+sft+s", command=Save_as_file)
+
+def Exit_fun(event=None):
+    global url, text_change
+    try:
+        if text_change:
+            mbox = messagebox.askyesnocancel(
+                "warning", " Do you want to save this file")
+            if mbox is True:
+                if url:
+                    content = text_editor.get(1.0, tk.END)
+                    with open(url, "w", encoding="utf-8") as for_read:
+                        for_read.write(content)
+                        main_app.destroy()
+                else:
+                    content_2 = str(text_editor.get(1.0, tk.END))
+                    url = filedialog.asksaveasfile(mode="w", defaultextension=".txt", filetypes=(
+                        ("Text file", "*.txt"), ("All files", "*.*")))
+                    url.write(content_2)
+                    url.close()
+                    main_app.destroy()
+            elif mbox is False:
+                main_app.destroy()
+        else:
+            main_app.destroy()
+    except:
+        return
+
+
+file.add_command(label="Exit", compound=tk.LEFT,
+                 accelerator="Ctrl+", command=Exit_fun)
+# Edit menu
+Edit = tk.Menu(menu, tearoff=False)
+menu.add_cascade(label="Edit", menu=Edit)
+Edit.add_command(label="Copy", compound=tk.LEFT, accelerator="Ctrl+C",
+                 command=lambda: text_editor.event_generate("<Control c>"))
+Edit.add_command(label="Cut", compound=tk.LEFT, accelerator="Ctrl+X",
+                 command=lambda: text_editor.event_generate("<Control x>"))
+Edit.add_command(label="Paste", compound=tk.LEFT, accelerator="Ctrl+v",
+                 command=lambda: text_editor.event_generate("<Control v>"))
+
+
+def find_fun(event=None):
+    def find():
+        word = find_input.get()
+        text_editor.tag_remove("match", "1.0", tk.END)
+        matches = 0
+
+        if word:
+            start_position = "1.0"
+            while True:
+                start_position = text_editor.search(
+                    word, start_position, stopindex=tk.END)
+                if not start_position:
+                    break
+                end_position = f"{start_position}+{len(word)}c"
+                text_editor.tag_add("match", start_position, end_position)
+                matches += 1
+                start_position = end_position
+                text_editor.tag_config(
+                    'match', foreground="red", background="blue")
+
+    find_popup = tk.Toplevel()
+    find_popup.geometry("300x150")
+    find_popup.title("find word")
+    find_popup.resizable(0, 0)
+
+    find_fram = ttk.Labelframe(find_popup, text="Find")
+    find_fram.pack(pady=20)
+
+    text_find = ttk.Label(find_fram, text="Find")
+    find_input = ttk.Entry(find_fram, width=30)
+    find_button = ttk.Button(find_fram, text="Find", command=find)
+    text_find.grid(row=0, column=0, padx=4, pady=4)
+    find_input.grid(row=0, column=1, padx=4, pady=4)
+    find_button.grid(row=2, column=0, padx=8, pady=4)
+
+
+Edit.add_command(label="Find", compound=tk.LEFT,
+                 accelerator="Ctrl+f", command=find_fun)
